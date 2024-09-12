86b524fd2967c3e1f004cc3adafae2a04babc2e4
Minordra <pantakran.n63@gmail.com>
2024-09-12 15:09:08 +0700

    Add files via upload

---
+++ phpbasic/Exercise5.php
@@ -0,0 +1,13 @@
+<?php
+    var_dump(test(3));
+    echo "<br>";
+    var_dump(test(14));
+    echo "<br>";
+    var_dump(test(12));
+    echo "<br>";
+    var_dump(test(37));
+
+    function test($n){
+        return $n % 3 == 0 || $n % 7 == 0;
+    }
+?>
